d36a169778b6575c5e1865d0a1b67a46e4e53be5
GuilhermeFranG <qhilherme@gmail.com>
2024-04-10 12:35:27 -0300

    Atividade 10 Básicas, Conversor de Temperatura - Lógica, DarkTheme.

---
+++ Atividades JS - Básicas/Atividade 10/ATBasic10.js
@@ -0,0 +1,80 @@
+var c = "°C";
+var f = "°F";
+var k = "K";
+
+var select1 = document.getElementById("tipoTemp1");
+var select2 = document.getElementById("tipoTemp2");
+select1.addEventListener('change', function () {
+    console.log(select1.value);
+});
+
+select2.addEventListener('change', function () {
+    console.log(select2.value);
+});
+
+var conversor = document.getElementById("conversor");
+
+
+function calcularTemp() {
+    if (select1.value === "Celsius" && select2.value === "Fahrenheit") {
+        let temp1 = document.getElementById("temp1").value;
+        let temp2 = document.getElementById("temp2").value = (temp1 * 9 / 5) + 32;
+        console.log(`${temp1} ${c} 
+${temp2} ${f}`);
+        console.log(`Conversão: (°C * 9 / 5) + 32`);
+        conversor.innerHTML = `<strong>Conversão:</strong> (${temp1} °C * 9 / 5) + 32 = ${temp2} °F`;
+    } else if (select1.value === "Fahrenheit" && select2.value === "Celsius") {
+        let temp1 = document.getElementById("temp1").value;
+        let temp2 = document.getElementById("temp2").value = ((temp1 - 32) * 5 / 9).toFixed(2);
+        console.log(`${temp1} ${f}  
+${temp2} ${c}`);
+        console.log(`Conversão: (°F - 32) *  5 / 9`);
+        conversor.innerHTML = `<strong>Conversão:</strong> (${temp1} °F - 32) *  5 / 9 = ${temp2} °C`;
+    } else if (select1.value === "Celsius" && select2.value === "Kelvin") {
+        let temp1 = document.getElementById("temp1").value;
+        let temp2 = document.getElementById("temp2").value = ((temp1 * 1 / 1) + 273.15).toFixed(2);
+        console.log(`${temp1} ${c}  
+${temp2} ${k}`);
+        console.log(`Conversão: °C + 273,15`);
+        conversor.innerHTML = `<strong>Conversão:</strong> ${temp1} °C + 273,15 = ${temp2} K`;
+    } else if (select1.value === "Kelvin" && select2.value === "Celsius") {
+        let temp1 = document.getElementById("temp1").value;
+        let temp2 = document.getElementById("temp2").value = (temp1 - 273.15).toFixed(2);
+        console.log(`${temp1} ${k}    
+${temp2} ${c}`);
+        console.log(`Conversão: K - 273,15`);
+        conversor.innerHTML = `<strong>Conversão:</strong> ${temp1} K - 273,15 = ${temp2} °C`;
+    } else if (select1.value === "Fahrenheit" && select2.value === "Kelvin") {
+        let temp1 = document.getElementById("temp1").value;
+        let temp2 = document.getElementById("temp2").value = ((temp1 - 32) * 5 / 9 + 273.15).toFixed(2);
+        console.log(`${temp1} ${f}    
+${temp2} ${k}`);
+        console.log(`Conversão: (°F - 32) * 5/9 + 273,15`);
+        conversor.innerHTML = `<strong>Conversão:</strong> (${temp1} °F - 32) * 5/9 + 273,15 = ${temp2} K`;
+    } else if (select1.value === "Kelvin" && select2.value === "Fahrenheit") {
+        let temp1 = document.getElementById("temp1").value;
+        let temp2 = document.getElementById("temp2").value = ((temp1 - 273.15) * 9 / 5 + 32).toFixed(2);
+        console.log(`${temp1} ${k}    
+${temp2} ${f}`);
+        console.log(`Conversão: (K - 273,15) * 9/5 + 32`);
+        conversor.innerHTML = `<strong>Conversão:</strong> (${temp1} K - 273,15) * 9/5 + 32 = ${temp2} °F`;
+    }
+}
+
+var theme = document.getElementById("theme");
+let isClicked = false;
+
+function changeTheme() {
+    if (!isClicked) {
+        theme.style.backgroundColor = "#000";
+        theme.style.color = "#fff";
+        theme.style.border = "1px solid #fff"
+        document.body.style.backgroundColor = "#000";
+        document.body.style.color = "#fff";
+    } else {
+        document.body.style.backgroundColor = "#fff";
+        document.body.style.color = "#000";
+    }
+
+    isClicked = !isClicked;
+}
--- Readme.md
+++ Readme.md
@@ -15,13 +15,14 @@ Esse repositório foi feito para a realização de Atividades de fixação, envo
 ### [Atividade 7: Objetos](https://github.com/GuilhermeFranG/AtividadesJavaScript/tree/main/Atividades%20JS%20-%20B%C3%A1sicas/Atividade%207)
 ### [Atividade 8: Manipulação de DOM](https://github.com/GuilhermeFranG/AtividadesJavaScript/tree/main/Atividades%20JS%20-%20B%C3%A1sicas/Atividade%208)
 ### [Atividade 9: Manipulação de Eventos](https://github.com/GuilhermeFranG/AtividadesJavaScript/tree/main/Atividades%20JS%20-%20B%C3%A1sicas/Atividade%209)
+### [Atividade 10: Conversor de Temperatura]()
 
 
 ## [**`Intermediário:`**](https://github.com/GuilhermeFranG/AtividadesJavaScript/blob/main/Atividades%20JS%20-%20Intermedi%C3%A1rio/Intermedi%C3%A1rio.txt)
 
 
 ### [Atividade 1: Calculadora de IMC](https://github.com/GuilhermeFranG/AtividadesJavaScript/tree/main/Atividades%20JS%20-%20Intermedi%C3%A1rio/Atividade%201)
-### Atividade 2: Contador de Palavras
+### [Atividade 2: Contador de Palavras](https://github.com/GuilhermeFranG/AtividadesJavaScript/tree/main/Atividades%20JS%20-%20Intermedi%C3%A1rio/Atividade%202)
 ### Atividade 3: Relógio Digital
 ### Atividade 4: Calculadora de Média
 ### Atividade 5: Conversor de Moeda
