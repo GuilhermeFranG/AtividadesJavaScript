e8f1c4bc6259c57de30f49373a88d61b0934a120
GuilhermeFranG <qhilherme@gmail.com>
2024-05-12 17:47:01 -0300

    Atividades JS intermediário atualizadas.

--- Atividades JS - Intermediário/Atividade 3/script.js
+++ Atividades JS - Intermediário/Atividade 3/script.js
@@ -1,12 +1,12 @@
-const getHours = () => {
+const getHours = () => { // const que engloba todo o horário 
 
     let tempoAtual = new Date();
     const horas = tempoAtual.getHours();
-    const hora = horas < 10 ? `0${horas}` : horas;
+    const hora = horas < 10 ? `0${horas}` : horas; // Se a hora < 10, adiciona o '0' na frente
     const minutos = tempoAtual.getMinutes();
-    const minuto = minutos < 10 ? `0${minutos}` : minutos;
+    const minuto = minutos < 10 ? `0${minutos}` : minutos; // Se os minutos < 10, adiciona o '0' na frente
     const segundos = tempoAtual.getSeconds();
-    const segundo = segundos < 10 ? `0${segundos}` : segundos;
+    const segundo = segundos < 10 ? `0${segundos}` : segundos; // Se os segundos < 10, adiciona o '0' na frente
 
     let horaFinal = `${hora}:${minuto}:${segundo}`;
     document.getElementById('time').innerHTML = horaFinal;
@@ -14,16 +14,16 @@ const getHours = () => {
 
 setInterval(() => {
     getHours();
-}, 1000)
+}, 1000) // Intervalo que atualiza o horário da página a cada 1s
 
 
-const getDate = () => {
+const getDate = () => { // const que engloba toda a data 
 
     let tempoAtual = new Date();
     const meses = tempoAtual.getMonth() + 1;
-    const mes = meses < 10 ? `0${meses}` : `${meses}/`;
+    const mes = meses < 10 ? `0${meses}` : meses; // Se o mês < 10, adiciona o '0' na frente
     const dias = tempoAtual.getDate();
-    const dia = dias < 10 ? `0${dias}` : `${dias}`;
+    const dia = dias < 10 ? `0${dias}` : dias; // Se o dia < 10, adiciona o '0' na frente
     const ano = tempoAtual.getFullYear();
 
 
@@ -33,4 +33,4 @@ const getDate = () => {
 
 setInterval(() => {
     getDate();
-}, 1000)
+}, 1000) // Intervalo que atualiza a data da página a cada 1s
