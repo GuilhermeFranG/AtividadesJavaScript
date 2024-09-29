4be8a322340fa7fb2c5e94d3e7ff4e6c4d66ab56
GuilhermeFranG <qhilherme@gmail.com>
2024-09-29 17:35:55 -0300

    Pequenos ajustes.

--- Atividades JS - Intermediário/Atividade 1/script.js
+++ Atividades JS - Intermediário/Atividade 1/script.js
@@ -5,7 +5,8 @@ function imc() {
     if (isNaN(result)) {
         alert('Por favor, insira um valor numérico para Peso e Altura!');
     } else {
-        document.getElementById("result").innerHTML = `${result.toFixed(2)} Kg/m<sup>2</sup>`;
+        document.getElementById("result").innerHTML = `Seu Indíce de Massa Corporal é:
+        ${result.toFixed(2)} Kg/m<sup>2</sup>`;
         // toFixed: define a quantidade de casas decimais
     }
 }   
